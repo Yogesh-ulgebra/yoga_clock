8dcc9a18d40731eb01a8673c244f00d7eecaa146
Yoga <yogatm7b@gmail.com>
2017-07-22 05:17:48 -0700

    success_clock

--- testingphp.php
+++ testingphp.php
@@ -6,27 +6,44 @@
 	<link rel="stylesheet" type="text/css" href="testingphp.css">
 	<script src="./jquery.js"></script>
 
+	<?php 
+
+		$h = (date('h')*60*60*6 + date('i')*60*6 + date('s')*6)/720;
+		echo $h.'<br>';
+		$h2 =$h+ 360;
+
+		$m = (date('i')*60*6 + date('s')*6)/60;
+		echo $m.'<br>';
+		$m2 =$m+ 360;
+		
+		$s = date('s')*6;
+		echo $s.'<br>';
+		$s2 =$s+ 360;
+
+
+	?>
+
 	<style type="text/css">
 		
-		@keyframes spin3 {
+		@keyframes spin1 {
   
-          0% { transform: rotatez(<?php echo "90"; ?>deg);}
-          100% { transform: rotatez(360deg);}
+          0% { transform: rotatez(<?php echo $h ?>deg);}
+          100% { transform: rotatez(<?php echo $h2; ?>deg);}
   
         }
         
 
-        @keyframes spin {
+        @keyframes spin2 {
   
-          0% { transform: rotatez(0deg);}
-          100% { transform: rotatez(360deg);}
+          0% { transform: rotatez(<?php echo $m; ?>deg);}
+          100% { transform: rotatez(<?php echo $m2; ?>deg);}
   
         }
 
-        @keyframes spin {
+        @keyframes spin3 {
   
-          0% { transform: rotatez(0deg);}
-          100% { transform: rotatez(360deg);}
+          0% { transform: rotatez(<?php echo $s; ?>deg);}
+          100% { transform: rotatez(<?php echo $s2; ?>deg);}
   
         }
 
--- testingphp2.php
+++ testingphp2.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>hi</title>
+	<link rel="stylesheet" type="text/css" href="testingphp2.css">
 </head>
 <body>
 
